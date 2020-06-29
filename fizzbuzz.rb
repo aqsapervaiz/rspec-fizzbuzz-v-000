a69fda0f805397742534188faa19e75561691852
Aqsa Pervaiz <aqsapervaiz@gmail.com>
2020-06-29 00:07:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz
+end
+
+def fizzbuzz(int)
+  if Integer / 3 == 0 
+    puts "Fizz"
+    
+end
+  
